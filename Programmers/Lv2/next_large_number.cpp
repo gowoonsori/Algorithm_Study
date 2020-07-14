f8dbd5bd227d361df6a7bcdd4b6e22dbda031326
gowoonsori <gowoonsori97@naver.com>
2020-07-14 20:49:33 +0900

    Update next_large_number.cpp

--- Programmers/Lv2/next_large_number.cpp
+++ Programmers/Lv2/next_large_number.cpp
@@ -17,11 +17,9 @@ int solution(int n) {
     int answer = n;
     int number_of_one_in_n = count_one( bitset<20>(n).to_string() );
 
-    cout << bitset<20>(n).to_string() <<endl;
     while(1){
         answer++;
         int numberof_one_in_answer = count_one(bitset<20>(answer).to_string());
-        cout << number_of_one_in_n << " : " <<numberof_one_in_answer<<endl;
         if(numberof_one_in_answer == number_of_one_in_n) break;    
     } 
     return answer;
