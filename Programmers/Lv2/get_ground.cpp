ed183ca87ead253d5946047d444a5dcec0681ba3
gowoonsori <gowoonsori97@naver.com>
2020-09-22 21:01:51 +0900

    Update get_ground.cpp

--- Programmers/Lv2/get_ground.cpp
+++ Programmers/Lv2/get_ground.cpp
@@ -8,12 +8,12 @@ int solution(vector<vector<int> > land)
 {
     int answer = 0;
     for(int i = 0; i < land.size()-1 ; i++){
-        land[i+1][0] += max(land[i][1],max(land[i][2],land[i][3]));
+        land[i+1][0] += max(land[i][1],max(land[i][2],land[i][3]));  
         land[i+1][1] += max(land[i][0],max(land[i][2],land[i][3]));
         land[i+1][2] += max(land[i][0],max(land[i][1],land[i][3]));
         land[i+1][3] += max(land[i][0],max(land[i][1],land[i][2]));
     }
-    answer = *max_element(land.end().end() - 4, land.end().end());
+    answer = max(land[land.size()-1][0], max(land[land.size()-1][1], max( land[land.size()-1][2], land[land.size()-1][3] )));       
     return answer;
 }
 int main(){
